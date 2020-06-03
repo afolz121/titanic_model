588f4eaac3bd2a398137f2e86464234a89ad7d84
afolz121 <anfolz556@gmail.com>
2020-06-03 12:34:12 -0500

    gitignore update. load train data

--- discovery.py
+++ discovery.py
@@ -3,5 +3,15 @@ import numpy as np
 import pandas as pd
 import os
 import sys
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+print(os.listdir())
+
+# %%
+train = pd.read_csv("train.csv")
+
+# %%
+train.head()
 
 # %%
