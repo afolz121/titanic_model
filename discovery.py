4921cc34d5ce61c6b0d0f0d34dc69b8b71f543e9
afolz121 <anfolz556@gmail.com>
2020-06-03 21:27:13 -0500

    changes

--- discovery.py
+++ discovery.py
@@ -12,10 +12,28 @@ print(os.listdir())
 train = pd.read_csv("train.csv")
 
 # %%
+sns.distplot(train['Age'])
+plt.show()
+
+# %%
 train.head()
 
 # %%
-sns.distplot(train['Age'])
-plt.show()
+def pipeline(df) :
+    df['under_5'] = np.where(df['Age'] >= 5,1,0)
+    df['Pclass_1'] = np.where(df['Pclass'] == 1,1,0)
+    return df
+
+
+# %%
+df = pipeline(train)
+
+# %%
+train[(train['Survived'] == 1)]['under_5'].value_counts()
+
+# %%
+for value in train.Pclass.unique() :
+   x =  train[(train['Survived'] == 1) & (train['Pclass'] == value)].shape[0] / train.shape[0]
+   print(value, str(x))
 
 # %%
