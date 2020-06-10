6f1f07841dfde1e46b1b5c0c74231184f90d665e
afolz121 <anfolz556@gmail.com>
2020-06-10 15:02:16 -0500

    create pipeline for cleasing

---
+++ cleanse_pipe.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+
+def pipeline(df) :
+    df['under_5'] = np.where(df['Age'] >= 5,1,0)
+    df['Pclass1'] = np.where(df['Pclass'] == 1,1,0)
+    df['cabinLetter'] = df['Cabin'].str[:1]
+    aa = [] 
+    cabinLetters = ['B','D','E','F']
+    for row in df['cabinLetter'] :
+        if row in cabinLetters :
+            aa.append(1)
+        else:
+            aa.append(0)
+    df['cabinLetter'] = aa
+    del[df['Cabin']]
+    df['Embarked'] = np.where(df['Embarked'] == 'C', 1, 0 )
+    return df
