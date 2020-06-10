81e81600f1f749a6a41c40807afb121a80fe7d85
afolz121 <anfolz556@gmail.com>
2020-06-10 15:33:56 -0500

    modeling 1st attempt

--- cleanse_pipe.py
+++ cleanse_pipe.py
@@ -1,6 +1,14 @@
 import numpy as np
 import pandas as pd
 
+def min_max(col) :
+    minimum = np.min(col)
+    maximum = np.max(col)
+    aa = []
+    for row in col :
+        aa.append((row - minimum) / (maximum - minimum))
+    return aa
+
 def pipeline(df) :
     df['under_5'] = np.where(df['Age'] >= 5,1,0)
     df['Pclass1'] = np.where(df['Pclass'] == 1,1,0)
@@ -17,4 +25,13 @@ def pipeline(df) :
     del[df['Ticket']]
     del[df['Name']]
     df['Embarked'] = np.where(df['Embarked'] == 'C', 1, 0 )
-    return df
+    target = df['Survived'].copy()
+    del[df['Survived']]
+    df['Sex'] = np.where(df['Sex'] == 'male', 1, 0)
+    del[df['PassengerId']]
+    df['Age'] = df['Age'].fillna(df['Age'].median())
+    for col in df.columns :
+        df[col] = min_max(df[col])
+
+    
+    return df, target
--- model.py
+++ model.py
@@ -4,14 +4,41 @@ import pandas as pd
 import os
 import sys
 from cleanse_pipe import pipeline
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+import math
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.svm import SVC
 
 # %%
 train = pd.read_csv('train.csv')
 
 # %%
-train = pipeline(train)
+data, target = pipeline(train)
 
 # %%
-train.head()
+dtree = DecisionTreeClassifier(max_depth= 4)
+dtree_fit = dtree.fit(data, target)
+
+dtree_predict = dtree_fit.predict(data)
+
+# %%
+print(classification_report(target, dtree_predict))
+
+# %%
+forest = RandomForestClassifier(max_depth= 4, n_estimators= 500)
+forest_fit = forest.fit(data, target)
+forest_predict = forest_fit.predict(data)
+
+# %%
+print(classification_report(target, forest_predict))
+
+# %%
+svmach = SVC(kernel= 'rbf', degree = 4)
+svmach_fit = svmach.fit(data, target)
+svmarch_predict = svmach_fit.predict(data)
+
+# %%
+print(classification_report(target, svmarch_predict))
 
 # %%
