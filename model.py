d782c0599a55098e182c6bb163c8e292e49ca131
Drew Folz <anfolz556@gmail.com>
2020-07-02 14:11:34 -0500

    New Discovery
    Discovery file now a jupyter notebook for readability. Still working on a model that can beat male/female...

--- model.py
+++ model.py
@@ -7,8 +7,9 @@ from cleanse_pipe import pipeline
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 import math
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import confusion_matrix, classification_report, roc_auc_score
 from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
 
 # %%
 train = pd.read_csv('train.csv')
@@ -28,12 +29,32 @@ dtree_predict = dtree_fit.predict(data)
 print(classification_report(target, dtree_predict))
 
 # %%
-forest = RandomForestClassifier(max_depth= 3, n_estimators= 100)
+forest = RandomForestClassifier(bootstrap=True, criterion='gini', max_depth= None, max_features = 'log2', min_samples_leaf= 5,
+                                min_samples_split = 2, n_estimators= 200)
 forest_fit = forest.fit(data, target)
 forest_predict = forest_fit.predict(data)
 
+#%%
+rforest = RandomForestClassifier()
+
+grid = {"max_depth": [None],
+              "max_features": [3,"sqrt", "log2"],
+              "min_samples_split": [n for n in range(1, 9)],
+              "min_samples_leaf": [5, 7],
+              "bootstrap": [False, True],
+              "n_estimators" :[200, 500],
+              "criterion": ["gini", "entropy"]}
+
+
+rforest_grid = GridSearchCV(rforest, param_grid = grid, cv=10, scoring="roc_auc", n_jobs= -1, verbose = 1)
+
+rforest_grid_fit = rforest_grid.fit(data, target)
+
+print(rforest_grid_fit.best_params_)
+
 # %%
 print(classification_report(target, forest_predict))
+print(roc_auc_score(target, forest_predict))
 
 # %%
 svmach = SVC(kernel= 'rbf', degree = 1, C = 10)
